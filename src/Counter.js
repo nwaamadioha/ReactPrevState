aae5595112695db8b00763aab87c0eae62e5e994
nwaamadioha <udokwu.temple960@gmail.com>
2022-11-02 17:05:22 +0100

    use prevCount function

--- src/Counter.js
+++ src/Counter.js
@@ -7,7 +7,7 @@ const Counter = () => {
     <section className='counter'>
         <p>{count}</p>
         <div className='buttons'>
-          <button onClick={() => setCount(count +1)}>+</button>
+          <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
           <button onClick={() => setCount(count -1)}>-</button>
         </div>
         <button className='reset' onClick={() => setCount(0)}>RESET</button>
